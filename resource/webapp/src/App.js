4329249c5007242afdc9e37bf6ece5d7ea9b0ff6
ngqtruong274 <ngqtruong274@gmail.com>
2023-05-11 20:44:38 +0700

    frontend board design

---
+++ resource/webapp/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import AppBar from "components/AppBar/AppBar";
+import BoardBard from "components/BoardBar/BoardBar";
+import BoardColumn from "components/BoardColumn/BoardColumn";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="trello-master">
+      <AppBar></AppBar>
+      <BoardBard></BoardBard>
+      <BoardColumn></BoardColumn>
+    </div>
+  );
+}
+
+export default App;
