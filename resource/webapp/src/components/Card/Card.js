986576f0b60391ab4e21b50685f3c6f3c9ca9ff0
19130267-DuongTruongVu <19130267@st.hcmuaf.edu.vn>
2023-05-12 02:34:17 +0700

    webapp column

---
+++ resource/webapp/src/components/Card/Card.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import "./Card.scss"
+
+export default function Card() {
+    return (
+        <div className="card-item">
+            <div className="card-detail">
+                <span className="card-title">dasdasdas</span>
+            </div>
+        </div>
+    )
+}
