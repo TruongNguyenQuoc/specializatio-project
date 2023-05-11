f285d1efffc4f802ad57265a3888a153fce1e481
19130267-DuongTruongVu <19130267@st.hcmuaf.edu.vn>
2023-05-11 21:26:58 +0700

    frontend column

---
+++ resource/webapp/src/components/BoardColumn/BoardColumn.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import "./BoardColumn.scss";
+
+export default function BoardColuumn() {
+  return <div className="board-content"></div>;
+}
