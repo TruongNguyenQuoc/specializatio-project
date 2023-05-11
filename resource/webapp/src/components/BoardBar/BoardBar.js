d91f09bbeef50662b91a426014e8a93ef362b22e
ntnhuy121 <19130272@st.hcmuaf.edu.vn>
2023-05-11 21:19:29 +0700

    frontend board

---
+++ resource/webapp/src/components/BoardBar/BoardBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./BoardBar.scss";
+
+export default function BoardBard() {
+  return (
+    <nav className="board-header">
+      <div className="header-content">
+        <span className="header-title">Clone App</span>
+      </div>
+    </nav>
+  );
+}
